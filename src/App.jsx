6bda67b5bf8040adbf49a8db9c1b5bfdee91fa30
ashifmahmud <mdamahmud2000@gmail.com>
2024-02-19 17:40:14 +0600

    nav done with some problem of resposive

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,31 @@ import List from "./components/layouts/List";
 import ListItem from "./components/layouts/ListItem";
 import { HiMiniBars3CenterLeft } from "react-icons/hi2";
 import Flex from "./components/Flex";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [show, setShow] = useState(true);
+
+  useEffect(() => {
+    function scrollWidth(e) {
+      if (window.innerWidth<1024) {
+        setShow(false);
+      }
+      else{
+        setShow(true)
+      }
+    }
+
+    window.addEventListener("resize", scrollWidth);
+  });
+
   return (
     <div className=" flex bg-red-200 max-w-container mx-auto">
       <div className="w-3/12 pl-3">
         <Image src={logo} />
       </div>
       <div className="w-9/12">
-      <HiMiniBars3CenterLeft className="ml-auto block lg:hidden md:hidden"/>
+        <HiMiniBars3CenterLeft onClick={(setShow) => setShow(!show)} className="ml-auto block lg:hidden md:hidden" />
         <List className="hidden lg:flex md:flex justify-center gap-x-10 font-dm text-sm font-regular">
           <ListItem className="hover:font-bold" item="Home" />
           <ListItem className="hover:font-bold" item="Shop" />
